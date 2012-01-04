0a14f8ef13f0204915d8648ccf383aaf4973e8b0
TracyZhang <tracy.garea@gmail.com>
2012-01-04 22:47:21 +0800

    Add kernel/panic.c comments.
    Signed-off-by: TracyZhang <tracy.garea@gmail.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -13,6 +13,12 @@
 
 void sys_sync(void);	/* it's really int */
 
+/* 该函数用来显示内核中出现的重大错误信息，并运行文件系统同步函数，然后
+ * 进入死循环 -- 死机.
+ * 如果当前进程是任务0 的话，还说明是交换任务出错，并且还没有运行文件系统同步函数
+ * 函数名前的关键字volatile用于告诉编译器gcc该函数不会返回。这样可让gcc产生更好一些
+ * 的代码，更重要的是使用这个关键字可以避免产生某些(未初始化变量的)假警告信息.
+ * 等同于现在gcc的函数属性说明：void panic(const char *s) __attribute__((noreturn)); */
 volatile void panic(const char * s)
 {
 	printk("Kernel panic: %s\n\r",s);
