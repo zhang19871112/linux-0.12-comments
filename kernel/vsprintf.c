fbe158b9b2accaa9303908dea09bfa86b3110c05
TracyZhang <tracy.garea@gmail.com>
2012-04-08 21:30:35 +0800

    Add kernel/exit.c comments.
    Signed-off-by: TracyZhang <tracy.garea@gmail.com>

--- kernel/vsprintf.c
+++ kernel/vsprintf.c
@@ -13,8 +13,10 @@
 #include <string.h>
 
 /* we use this so that we can do without the ctype library */
-#define is_digit(c)	((c) >= '0' && (c) <= '9')
+#define is_digit(c)	((c) >= '0' && (c) <= '9')	/* 判断字符c是否为数字字符 */
 
+/* 该函数将字符数字转换成整数, 输入是数字串指针的指针, 返回是结果数值。另外指针
+ * 将前移 */
 static int skip_atoi(const char **s)
 {
 	int i=0;
@@ -24,19 +26,22 @@ static int skip_atoi(const char **s)
 	return i;
 }
 
-#define ZEROPAD	1		/* pad with zero */
-#define SIGN	2		/* unsigned/signed long */
-#define PLUS	4		/* show plus */
-#define SPACE	8		/* space if plus */
-#define LEFT	16		/* left justified */
-#define SPECIAL	32		/* 0x */
-#define SMALL	64		/* use 'abcdef' instead of 'ABCDEF' */
+/* 这里定义转换类型的各种符号常数 */
+#define ZEROPAD	1		/* pad with zero，填充0 */
+#define SIGN	2		/* unsigned/signed long，无符号/符号长整数 */
+#define PLUS	4		/* show plus，显示加 */
+#define SPACE	8		/* space if plus，如是加，则置空格 */
+#define LEFT	16		/* left justified，左调整 */
+#define SPECIAL	32		/* 0x，0x */
+#define SMALL	64		/* use 'abcdef' instead of 'ABCDEF'，使用小写字母 */
 
+/* 除操作。输入：n为被除数，base为除数；结果：n为商，函数返回值为余数 */
 #define do_div(n,base) ({ \
 int __res; \
 __asm__("divl %4":"=a" (n),"=d" (__res):"0" (n),"1" (0),"r" (base)); \
 __res; })
 
+/* 将整数转换为指定进制的字符串 */
 static char * number(char * str, int num, int base, int size, int precision
 	,int type)
 {
@@ -89,6 +94,9 @@ static char * number(char * str, int num, int base, int size, int precision
 	return str;
 }
 
+/* 下面函数是送格式化输出到字符串中
+ * 为了能在内核中使用格式化输出，Linus在内核实现乐该C标准函数
+ * fmt是格式字符串，args是个数变化的值，buf是输出字符串缓冲区 */
 int vsprintf(char *buf, const char *fmt, va_list args)
 {
 	int len;
